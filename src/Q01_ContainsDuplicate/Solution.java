eb789724b6b0c743b0c2aa2f82324fddace0b0ec
Yavuz Ozmen <yvz.o@Yavuz-MacBook-Pro.local>
2025-04-13 12:18:37 +0200

    Add README with Two Sum screenshot

--- src/Q01_ContainsDuplicate/Solution.java
+++ src/Q01_ContainsDuplicate/Solution.java
@@ -1,12 +1,14 @@
 package Q01_ContainsDuplicate;
 
+import com.sun.jdi.request.StepRequest;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
 
-        int[] nums = {2, 3, 6, 7, 9, 11, 2};
+        int[] nums = {1, 2, 3, 54, 6, 8, 2, 44};
 
         Solution s = new Solution();
         boolean result = s.containsDuplicate(nums);
@@ -16,7 +18,6 @@ public class Solution {
     public boolean containsDuplicate(int[] nums) {
 
         Set<Integer> seen = new HashSet<>();
-
         for (int num : nums) {
             if (seen.contains(num)) {
                 return true;
@@ -25,6 +26,22 @@ public class Solution {
         }
         return false;
     }
-
 }
 
+
+// int[] nums = {2, 3, 6, 7, 9, 11, 2};
+//
+//        Solution s = new Solution();
+//        boolean result = s.containsDuplicate(nums);
+//        System.out.println("Sonuc : " + result);
+
+//  Set<Integer> seen = new HashSet<>();
+//
+//        for (int num : nums) {
+//            if (seen.contains(num)) {
+//                return true;
+//            }
+//            seen.add(num);
+//        }
+//        return false;
+//    }
